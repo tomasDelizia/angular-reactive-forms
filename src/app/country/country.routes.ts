aadc81fff94d15b8fed375bb6f883137c4d64a6d
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-10 09:10:30 +0100

    add routing for auth, country, and reactive modules with lazy loading

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: CountryPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'sign-up',
+      },
+    ],
+  },
+];
