ca33cc72442d309b9782ab9f6d001f84e8ba33a4
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-10 09:53:38 +0100

    implement reactive form in basic page component

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,16 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent {
+  form = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStock: new FormControl(0),
+  });
+}
