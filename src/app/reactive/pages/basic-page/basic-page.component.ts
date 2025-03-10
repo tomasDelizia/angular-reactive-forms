0fc789e476a16d03fbb554a091390abcb318ce50
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-10 10:52:23 +0100

    refactor basic page component to use FormBuilder for reactive form initialization

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -8,9 +8,25 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
-  form = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStock: new FormControl(0),
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    name: [
+      '',
+      [
+        /** Validadores síncronos */
+      ],
+      [
+        /** Validadores asíncronos */
+      ],
+    ],
+    price: [0],
+    inStock: [0],
   });
+
+  // form = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStock: new FormControl(0),
+  // });
 }
