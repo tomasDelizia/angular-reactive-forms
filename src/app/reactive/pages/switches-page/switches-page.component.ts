fad0fd2defc060e93250b7af20882a4e686ff0c0
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-10 13:11:23 +0100

    implement reactive form with validation and submission for switches page component

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,30 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  form: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantsNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.markAllAsTouched();
+  }
+}
