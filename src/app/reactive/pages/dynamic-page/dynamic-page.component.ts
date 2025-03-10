cd00ac6f340678b9c0417d1cafc8787b9243e231
tdeliziae <tdeliziae@eservicios.indra.es>
2025-03-10 12:10:22 +0100

    implement reactive form with validation for dynamic page component using formArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,32 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent {}
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+  form: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear Solid', Validators.required],
+        ['Ratchet & Clank', Validators.required],
+      ],
+      Validators.minLength(3)
+    ),
+  });
+
+  get favoriteGames(): FormArray {
+    return this.form.get('favoriteGames') as FormArray;
+  }
+}
